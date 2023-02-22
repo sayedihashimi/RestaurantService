a356233f609fab9feb207005d22dd0b944cfc243
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-02-22 14:22:25 -0500

    model updates

--- RestaurantService.Api/TogoOrder.cs
+++ RestaurantService.Api/TogoOrder.cs
@@ -2,7 +2,7 @@
     public class TogoOrder {
         public int Id { get; set; }
         public DateTime? OrderCreated { get; set; }
-        public List<MenuItem>? ItemsOrdered { get; set; }
+        public List<MenuItemOrdered>? ItemsOrdered { get; set; }
         public Decimal? Subtotal { get; set; }
         public Decimal? Tax { get; set; }
         public Decimal? Total { get; set; }
