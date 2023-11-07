bcd2813fb7bd8a3322d89a7a5fb9933e92b10f6b
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-11-07 13:27:39 -0500

    after scaffolding Contact

---
+++ src-v2/MyRestaurantApi/MyRestaurantApi/Data/MyRestaurantApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyRestaurantApi;
+
+namespace MyRestaurantApi.Data
+{
+    public class MyRestaurantApiContext : DbContext
+    {
+        public MyRestaurantApiContext (DbContextOptions<MyRestaurantApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyRestaurantApi.Contact> Contact { get; set; } = default!;
+    }
+}
--- src-v2/MyRestaurantApi/MyRestaurantApi/Program.cs
+++ src-v2/MyRestaurantApi/MyRestaurantApi/Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRestaurantApi.Data;
+using MyRestaurantApi;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MyRestaurantApiContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MyRestaurantApiContext") ?? throw new InvalidOperationException("Connection string 'MyRestaurantApiContext' not found.")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,6 +39,8 @@ app.MapGet("/weatherforecast", () => {
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.MapContactEndpoints();
 
 app.Run();
 
