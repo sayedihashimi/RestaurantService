e24144f0c66ed96fd46c226a0358787087635c3a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-11-13 21:39:16 -0800

    removing weatherforecast

--- src-v2/MyRestaurantApi/MyRestaurantApi/Program.cs
+++ src-v2/MyRestaurantApi/MyRestaurantApi/Program.cs
@@ -4,7 +4,8 @@ using MyRestaurantApi.Data;
 using MyRestaurantApi;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MyRestaurantApiContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("MyRestaurantApiContext") ?? throw new InvalidOperationException("Connection string 'MyRestaurantApiContext' not found.")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("MyRestaurantApiContext") ?? 
+    throw new InvalidOperationException("Connection string 'MyRestaurantApiContext' not found.")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,25 +22,6 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapContactEndpoints();
 
 app.MapAdminContactEndpoints();
@@ -47,7 +29,3 @@ app.MapAdminContactEndpoints();
 app.MapMenuItemEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
