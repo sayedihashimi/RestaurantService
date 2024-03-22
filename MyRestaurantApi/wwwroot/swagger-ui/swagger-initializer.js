b8e3eed870264446ba90db2ad8ce426553a40a99
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-03-22 14:13:23 -0400

    formatting swagger-initializer.js

--- MyRestaurantApi/wwwroot/swagger-ui/swagger-initializer.js
+++ MyRestaurantApi/wwwroot/swagger-ui/swagger-initializer.js
@@ -1,20 +1,20 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+window.onload = function () {
+    //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "https://localhost:7293/swagger/v1/swagger.json",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+    // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+    window.ui = SwaggerUIBundle({
+        url: "https://localhost:7293/swagger/v1/swagger.json",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout",
+    });
 
-  //</editor-fold>
+    //</editor-fold>
 };
