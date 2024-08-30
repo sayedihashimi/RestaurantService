bc39883e6945801939d45812075ccb28f7f5e932
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-08-30 11:01:16 -0400

    Updating MenuItem to have a more/less button on the description

--- restaurantweb/src/App.jsx
+++ restaurantweb/src/App.jsx
@@ -8,14 +8,15 @@ function App() {
     useEffect(() => {
         populateWeatherData();
     }, []);
-    const contents = menuItems === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <Menu items={menuItems} />;
+
     return (
         <div>
-            <h1 id="tableLabel">Menu Items</h1>
-
-            {contents}
+            <h1>Menu Items</h1>
+            {menuItems === undefined ? (
+                <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+            ) : (
+                <Menu items={menuItems} />
+            )}
         </div>
     );
 
--- restaurantweb/src/components/MenuItem.jsx
+++ restaurantweb/src/components/MenuItem.jsx
@@ -1,14 +1,19 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-function MenuItem({ id, name, emojiName, price, description, category }) {
+function MenuItem({ id, name, emojiName, price, description, category, truncate = true }) {
+    const [truncateDescription, setTruncateDescription] = useState(truncate);
+
+    let desc = truncateDescription ? description.slice(0, 50) + '...' : description;
+
     return (
-        <div className="menu-item" id={id} data-menu-category={category}>
-            <h2>{name}</h2>
-            <h3>{emojiName}</h3>
-            <p>{description}</p>
-            <p>Price: ${price?.toFixed(2)}</p>
-        </div>
+            <div className="menu-item" id={id} data-menu-category={category}>
+                <h2>{name}</h2>
+                <h3>{emojiName}</h3>
+                <p>{desc}</p>
+                <p>Price: ${price?.toFixed(2)}</p>
+                <button onClick={()=>setTruncateDescription((previous)=>!previous)}>{truncateDescription ? 'More' : 'Less'}</button>
+            </div>
     );
 }
 
@@ -19,6 +24,7 @@ MenuItem.propTypes = {
     price: PropTypes.number,
     description: PropTypes.string,
     category: PropTypes.string,
+    truncate: PropTypes.bool,
 };
 
 export default MenuItem;
