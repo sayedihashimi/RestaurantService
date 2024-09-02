edd297ef6c6ec04c1a3723ac8667af11bf830713
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-09-02 00:12:42 -0400

    Refactored menu - Updated Menu to be self-contained including getting data - Added a favicon

--- restaurantweb/src/App.jsx
+++ restaurantweb/src/App.jsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import Menu from './components/Menu';
 
 function App() {
-    const [menuItems, setMenuItems] = useState();
-    useEffect(() => {
-        populateMenuItems();
-    }, []);
-
     return (
-        <div>
-            <h1>Menu Items</h1>
-            {menuItems === undefined ?
-                (<p><em>Loading... Waiting for backend to start, try again soon.</em></p>) :
-                (<Menu items={menuItems} />)
-            }
-        </div>
+        <Menu />
     );
-
-    async function populateMenuItems() {
-        const response = await fetch('api/menuitem');
-        const data = await response.json();
-        setMenuItems(data);
-    }
 }
 export default App;
--- restaurantweb/src/components/Menu.jsx
+++ restaurantweb/src/components/Menu.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import PropTypes from 'prop-types';
 import MenuItem from './MenuItem';
 import './Menu.css';
 
-function Menu({ items }) {
+function Menu() {
+    const [menuItems, setMenuItems] = useState();
+    useEffect(() => {
+        populateMenuItems();
+    }, []);
+
     return (
-        <div className="menu-grid">
-            {items.map(item => (
-                <MenuItem key={item.id} menuItem={item} />
-            ))}
+        <div id="menuContainer">
+        { !menuItems ?
+            (<p><em>Loading... Waiting for backend to start, try again soon.</em></p>) :
+            (
+                <div className="menu-grid">
+                    {menuItems.map(item => (
+                        <MenuItem key={item.id} menuItem={item} />
+                    ))}
+                 </div>
+            )
+        }
         </div>
     );
-}
 
-Menu.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            name: PropTypes.string,
-            emojiName: PropTypes.string,
-            price: PropTypes.number,
-            description: PropTypes.string,
-            category: PropTypes.number,
-        })
-    ).isRequired,
-};
+    async function populateMenuItems() {
+        const response = await fetch('api/menuitem');
+        const data = await response.json();
+        setMenuItems(data);
+    }
+}
 
 export default Menu;
