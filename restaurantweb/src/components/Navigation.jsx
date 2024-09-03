390e2c9f3bdc3405f76cc1251674282e0048bf24
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-09-03 00:23:29 -0400

    changing nav to use CommandBar

--- restaurantweb/src/components/Navigation.jsx
+++ restaurantweb/src/components/Navigation.jsx
@@ -1,33 +1,76 @@
 import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Navigation.css';
 import { Link } from '@fluentui/react';
-import { DefaultButton } from '@fluentui/react';
+import { DefaultButton, CommandBar } from '@fluentui/react';
+import { FocusTrapZone } from '@fluentui/react/lib/FocusTrapZone';
 
 function Navigation() {
-  return (
-      <nav>
-          <ul>
-              <li><NavLink to='/' component={Link}>
-                  <DefaultButton text="Home" /></NavLink></li>
-              <li><NavLink to='/menu' component={Link}>
-                  <DefaultButton text="Menu" /></NavLink></li>
-              <li><NavLink to='/about' component={Link}>
-                  <DefaultButton text="About" /></NavLink></li>
-              <li><NavLink to='/contact' component={Link}>
-                  <DefaultButton text="Contact" /></NavLink></li>
-          </ul>
-      </nav>
+    const navigate = useNavigate();
 
-
-      // <nav>
-      //    <ul>
-      //        <li><NavLink to='/' component={Link}>Home</NavLink></li>
-      //        <li><NavLink to='/menu' component={Link}>Menu</NavLink></li>
-      //        <li><NavLink to='/about' component={Link}>About</NavLink></li>
-      //        <li><NavLink to='/contact' component={Link}>Contact</NavLink></li>
-      //    </ul>
-      //</nav>
-  );
+    const items = [
+        {
+            key: 'home',
+            text: 'Home',
+            iconProps: { iconName: 'HomeSolid' },
+            onClick: () => navigate('/'),
+        },
+        {
+            key: 'menu',
+            text: 'Menu',
+            iconProps: { iconName: 'ContextMenu' },
+            onClick: () => navigate('/menu'),
+        },
+        {
+            key: 'contact',
+            text: 'Contact',
+            iconProps: { iconName: 'ContactInfo' },
+            onClick: () => navigate('/contact'),
+        },
+    ];
+    // TODO: far items don't do anything yet, they should do something.
+    const _farItems = [
+        {
+            key: 'tile',
+            text: 'Grid view',
+            // This needs an ariaLabel since it's icon-only
+            ariaLabel: 'Grid view',
+            iconOnly: true,
+            iconProps: { iconName: 'Tiles' },
+            onClick: () => console.log('Tiles'),
+        },
+        {
+            key: 'info',
+            text: 'Info',
+            ariaLabel: 'Info',
+            iconOnly: true,
+            iconProps: { iconName: 'Info' },
+            onClick: () => console.log('Info'),
+        },
+    ];
+    return (
+        <nav>
+            <CommandBar
+                items={items}
+                farItems={_farItems}
+                styles={{
+                    root: {
+                        padding: '.7rem 1.4rem', // Add vertical padding to the CommandBar
+                    },
+                    primarySet: {
+                        gap: '.7rem', // Add gap between primary items
+                    },
+                    secondarySet: {
+                        gap: '.7rem', // Add gap between far items
+                    },
+                    item: {
+                        margin: '0 .7rem', // Add horizontal margin to each item
+                        padding: '.35rem 0', // Add vertical padding to each item
+                    },
+                }}
+            />
+        </nav>
+    );
 }
 
 export default Navigation;
--- restaurantweb/src/main.jsx
+++ restaurantweb/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+
+initializeIcons();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
