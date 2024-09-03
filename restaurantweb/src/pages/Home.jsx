832d06a06dfabcb003672285c405b60efe336b51
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-09-03 00:55:01 -0400

    adding some content to home page

--- restaurantweb/src/pages/Home.jsx
+++ restaurantweb/src/pages/Home.jsx
@@ -1,6 +1,13 @@
+import logo from '../../public/the-cozy-plate-logo.svg';
+import interior from '../../public/restaurant-interior-01.webp';
+import './Home.css'
+
 function Home() {
   return (
-    <p>Home</p>
+      <div>
+          <img src={logo} alt="The Cozy Plate Logo" className="logo" />
+          <img src={interior} alt="The Cozy Plate interior" className="interior-image" />
+      </div>
   );
 }
 
