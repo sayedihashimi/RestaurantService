7f4e3f28f16907dc42e24f34cad56c8c18f83242
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-09-07 00:24:54 -0400

    adding a CustomerInfo component with styling

--- restaurantweb/src/components/NewOrder.jsx
+++ restaurantweb/src/components/NewOrder.jsx
@@ -1,48 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import CustomerInfo from './CustomerInfo';
 function NewOrder() {
-    const [contacts, setContacts] = useState([]);
-    const [selectedContact, setSelectedContact] = useState('');
-    const [selectedContactDetails, setSelectedContactDetails] = useState(null);
 
-    useEffect(() => {
-        // Fetch the contacts from the API
-        fetch('/api/contact')
-            .then(response => response.json())
-            .then(data => setContacts(data))
-            .catch(error => console.error('Error fetching contacts:', error));
-    }, []);
-
-    useEffect(() => {
-        // Update selectedContactDetails when selectedContact changes
-        const contact = contacts.find(contact => contact.id === parseInt(selectedContact));
-        setSelectedContactDetails(contact);
-    }, [selectedContact, contacts]);
-
-    const handleContactChange = (event) => {
-        setSelectedContact(event.target.value);
-    };
     return (
-        <div className="customerContainer">
-            <p>Customer name:&nbsp;
-            <select value={selectedContact} onChange={handleContactChange}>
-                <option value="">Select a customer</option>
-                {contacts.map(contact => (
-                    <option key={contact.id} value={contact.id}>
-                        {contact.name}
-                    </option>
-                ))}
-            </select></p>
-
-            {selectedContactDetails && (
-                <ul>
-                    {Object.entries(selectedContactDetails).map(([key, value]) => (
-                        <li key={key}>
-                            <strong>{key}:</strong> {value}
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
+        <CustomerInfo />
     );
 }
 
